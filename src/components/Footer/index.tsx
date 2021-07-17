8bf163552a89641a43c9270981b075b9b1bdc51d
Diego Silva <dev.diegosilva@hotmail.com>
2021-07-17 01:51:46 -0300

    feat: :sparkles: Footer
    footer with contacts and addres done

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import * as S from "./style";
+import LogosFooter from "assets/logosFooter.svg";
+import phoneIcon from "assets/phoneIcon.svg";
+import msgIcon from "assets/msgIcon.svg";
+
+const Baseboard = () => {
+  return (
+    <S.Footer>
+      <S.AddressContainer style={{ color: "white" }}>
+        <h2>Localização</h2>
+        <div>Avenida Andrômeda, 2000. Bloco 6 e 8 </div>
+        <div>Alphavile SP</div>
+        <div>brasil@corebiz.ag</div>
+        <div>+55 11 3090 1039</div>
+      </S.AddressContainer>
+      <S.ContactsContainer>
+        <S.ContactButton>ENTRE EM CONTATO</S.ContactButton>
+        <S.ContactButton>
+          FALE COM O NOSSO
+          <br /> CONSULTOR ONLINE
+        </S.ContactButton>
+      </S.ContactsContainer>
+      <S.LogosContainer>
+        <img src={LogosFooter} alt="logos footer" />
+      </S.LogosContainer>
+    </S.Footer>
+  );
+};
+
+export default Baseboard;
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 213px;
+  background-color: #000;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const AddressContainer = styled.div`
+  height: 213px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const ContactsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContactButton = styled.button`
+  height: 38px;
+  border-radius: 8px;
+  border: none;
+  background-color: #fff;
+  color: #000;
+  text-align: center;
+  font-family: Nunito;
+  font-weight: 600;
+  font-size: 12px;
+  margin-bottom: 20px;
+`;
+
+export const LogosContainer = styled.div``;
